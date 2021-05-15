79011d23fd69829847f22d44a34f35b17ccf6d91
Olivier <olivierots@hotmail.com>
2021-05-15 02:26:16 +0100

    Create oop_qa_basic.py

---
+++ oop_qa_basic.py
@@ -0,0 +1,19 @@
+# challenege 1
+# first value of i
+#i=1
+#while i<=10:
+#  print(i,"hi")
+#  i=i+1.  # fnal value of i
+  
+
+## challenege 2   
+T=int(input("enter any number:"))
+
+i=1.  # initial i value
+
+while i<=10:   
+  # as long as the condition above is met,
+  # perform the below operations repeatedly 
+  
+  print(T,"x",i,"=", (T*i))
+  i=i+1. 
