a9f80bfe99016ab2eecd4ed4e39ba1351bc56109
Olivier <olivierots@hotmail.com>
2021-05-15 02:30:31 +0100

    Create python-exercises-qa.py

---
+++ python-exercises-qa.py
@@ -0,0 +1,29 @@
+# The input() method reads a line from input,converts into a string and returns it.
+# string = non numeric value
+# int do not accept decimal digits whereas float does
+
+1.
+#physics = 90
+#chemistry = 89
+#maths = 78
+#total = physics + chemistry + maths
+#print(total)
+
+2.
+#qty=10
+#price=100
+#bill=qty*price
+#print("your bill is: ", bill,"pounds")
+
+#A=input ("please enter first number:")
+#B=input ("please enter 2nd number:")
+#print("--------------")
+#print("The first value you entered was", A)
+#print("The 2nd value you entered was", B)
+
+4.
+#A=input ("please enter first number:")
+#B=input ("please enter 2nd number:")
+#print("--------------")
+#result=int(A)*int(B)
+#print("Answer is:", result)
