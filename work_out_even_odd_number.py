3e58480b818939410fbb7b4279eef1dca675cf36
Olivier <olivierots@hotmail.com>
2021-05-15 02:25:13 +0100

    Create work_out_even_odd_number.py

---
+++ work_out_even_odd_number.py
@@ -0,0 +1,6 @@
+number=int(input("enter any number: "))
+
+if number%2==0:   # this basically means its not a decimal digit 
+  print(number, "is an even No")
+else:
+  print(number, "is an Odd No")
