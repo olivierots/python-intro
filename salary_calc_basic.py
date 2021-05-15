189d63130a7fec51913cda0a63163c504555eef5
Olivier <olivierots@hotmail.com>
2021-05-15 02:27:38 +0100

    Create salary_calc_basic.py

---
+++ salary_calc_basic.py
@@ -0,0 +1,13 @@
+salary=float(input("Enter your salary:" ))
+
+if salary>=2000:
+   tax=salary*21/100
+   net=salary-tax
+
+else:
+   tax=salary*15/100
+   net=salary-tax
+
+print("your salary is:", salary)
+print("your tax is:", tax)
+print("your net salary is:", net)
